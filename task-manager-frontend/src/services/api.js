881a98000c957dc416949a6f56762975589927e6
Parth-2310 <aditigupta0963@gmail.com>
2026-05-21 12:14:17 +0530

    Updated backend

--- task-manager-frontend/src/services/api.js
+++ task-manager-frontend/src/services/api.js
@@ -1,6 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
+
+const API_BASE_URL = 'https://secure-task-manager-nyix.onrender.com/api';
+
 const api = axios.create({
-    baseURL: "https://secure-task-manager-nyix.onrender.com/api",
-    withCredentials: true,
+  baseURL: API_BASE_URL,
+  withCredentials: true,
 });
+
 export default api;
