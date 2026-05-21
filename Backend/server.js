bd897053f1c172ce046faca80eea0545591a9a1f
Parth-2310 <aditigupta0963@gmail.com>
2026-05-21 14:26:56 +0530

    Updated cors

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use(morgan('dev'));
 const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
-  'https://secure-task-manager-axoqf09g1-aditi-gupta-s-projects.vercel.app'
+  'https://secure-task-manager-7njd2ukf4-aditi-gupta-s-projects.vercel.app'
 ];
 
 app.use(cors({
