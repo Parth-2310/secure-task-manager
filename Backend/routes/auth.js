77b76007221f3e0b45589a56f14681c28ce9ebbe
Parth-2310 <aditigupta0963@gmail.com>
2026-05-29 13:09:18 +0530

    Improved backend

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -65,11 +65,11 @@ router.post('/login', async (req, res) => {
     );
 
     console.log('Setting cookie now')
-    
+
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
